f27ec287efcac23da2736f0ac94deea5326e825f
Anirudh <arhanchinamani0909@gmail.com>
2024-03-27 17:48:16 +0530

    Flatmap

---
+++ the_only_dart_file/bin/flatMap.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+
+  
+  // flatmapping optionals => (Unwrap and map it)
+  String? firstName = 'Anirudh';
+  String? lastName = 'R H';
+
+  final fullName = firstName.flatmap(
+        (f) => lastName.flatmap(
+          (l) => '$f $l',
+        ),
+      ) ??
+      'Either first or last or both name are null';
+  print(fullName);
+}
+
+extension Flatmat<T> on T? {
+  R? flatmap<R>(
+    R? Function(T) callback,
+  ) {
+    final shadow = this;
+    if (shadow == null) {
+      return null;
+    } else {
+      return callback(shadow);
+    }
+  }
+}
--- the_only_dart_file/bin/optionals.dart
+++ the_only_dart_file/bin/optionals.dart
@@ -1,7 +1,8 @@
 void main(List<String> args) {
   // optionals (nullable values)
   int? a;
-  print(a); // if this wasn't a optional datatype then we'd be getting an error stating
+  print(
+      a); // if this wasn't a optional datatype then we'd be getting an error stating
   // non nullable local variable must be initialized before it's used
 
   String? name;
@@ -15,7 +16,8 @@ void main(List<String> args) {
 
   String? name1;
   // name1 = 'AN';
-  name1 ??= 'Anirudh'; // ??= is a null-coalescing assignment operator => meaning it assigns the
+  name1 ??=
+      'Anirudh'; // ??= is a null-coalescing assignment operator => meaning it assigns the
   //                                                 values of RHS only if the
   //                                                 LHS evaluates to null
   print(name1);
@@ -41,11 +43,58 @@ void main(List<String> args) {
   // #1      _delayEntrypointInvocation.<anonymous closure> (dart:isolate-patch/isolate_patch.dart:295:33)
   // #2      _RawReceivePort._handleMessage (dart:isolate-patch/isolate_patch.dart:184:12)
 
-
+/*(Dont forget to comment the function below if you're uncommenting this)
   String getFullName() {
     return 'Anirudh H';
   } 
 
+  String? getFullNameOptional(){
+    return null;
+  }
+
   String? fullName = getFullName();
   print(fullName);
+
+  // conditional statements (apart from ==,<=,>= )
+  if(fullName?.contains('Anirudh H') ?? false){
+    print('full Name contains Anirudh H');
+  } else{
+    print('No Anirudh H in fullName');
+  }
+
+  final naam = getFullNameOptional() ?? getFullName();
+  print(naam);
+
+
+  // extending optional Types
+  final someName = getFullNameOptional();
+  someName.describe();
+*/
+
+  // unwrapping multiple optionals
+
+  print(getFullName(null, null));
+  print(getFullName('Anirudh', 'R H'));
+  print(getFullName('Anirudh', null));
+
+
 }
+
+extension Describe on Object? {
+  void describe() {
+    if (this == null) {
+      print("This is Null");
+    } else {
+      print('This isnt null');
+    }
+  }
+}
+
+T? withAll<T>(
+  List<T?> optionals,
+  T? Function(List<T>) callback,
+) =>
+    optionals.any((e) => e == null) ? null : callback(optionals.cast<T>());
+
+String getFullName(String? firstName, String? lastName) =>
+    withAll([firstName, lastName], (names) => names.join(' ')) ?? 'Empty';
